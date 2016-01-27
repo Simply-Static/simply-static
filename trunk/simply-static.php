001c23c364934937f93ea39c5a8fb1ef29f28641
Scott Blaine <scott@scottblaine.com>
2016-01-26 21:15:20 -0600

    prepping for v1.2.2

--- trunk/simply-static.php
+++ trunk/simply-static.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Simply Static
  * Plugin URI:        http://codeofconduct.co/simply-static
  * Description:       Produces a static HTML version of your WordPress install and adjusts URLs accordingly.
- * Version:           1.2.1
+ * Version:           1.2.2
  * Author:            Code of Conduct
  * Author URI:        http://codeofconduct.co/
  * License:           GPL-2.0+
