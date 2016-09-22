24148e5a48e52339e1ec115b4076c67daf2266f4
Scott Blaine <scott@scottblaine.com>
2016-09-22 01:29:02 -0500

    [#143] Wildcard SQL grants are now properly identified during SQL permission checks

--- includes/class-simply-static-sql-permissions.php
+++ includes/class-simply-static-sql-permissions.php
@@ -60,11 +60,21 @@ class Simply_Static_Sql_Permissions {
 			// Loop through all of the grants and set permissions to true where
 			// we're able to find them.
 			foreach ( $rows as $row ) {
+				// Find the database name
 				preg_match( '/GRANT (.+) ON (.+) TO/', $row[0], $matches );
 				// Removing backticks and backslashes for easier matching
 				$db_name = preg_replace('/[\\\`]/', '', $matches[2]);
-				// Check for matches for all dbs (*.*) or this specific WP db
-				if ( in_array( $db_name, array( '*.*', $wpdb->dbname . '.*' ) ) ) {
+
+				if ( substr( $db_name, -3 ) == '%.*' ) {
+					// Check for a wildcard match on the database
+					$db_name = substr( $db_name, 0, -3 );
+					$db_name_match = ( stripos( $wpdb->dbname, $db_name ) === 0 );
+				} else {
+					// Check for matches for all dbs (*.*) or this specific WP db
+					$db_name_match = in_array( $db_name, array( '*.*', $wpdb->dbname . '.*' ) );
+				}
+
+				if ( $db_name_match ) {
 					foreach ( explode( ',', $matches[1] ) as $permission ) {
 						$permission = str_replace( ' ', '_', trim( strtolower( $permission ) ) );
 						if ( $permission === 'all_privileges' ) {
