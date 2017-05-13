0fdf1dafdddf94eaa6c458320d574c05ab0d4ed6
Scott Blaine <scott@scottblaine.com>
2017-05-12 20:15:09 -0500

    prepping for v2.1.0

--- simply-static.php
+++ simply-static.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Plugin Name:       Simply Static
  * Plugin URI:        http://simplystatic.co/
  * Description:       Produces a static HTML version of your WordPress install and adjusts URLs accordingly.
- * Version:           2.0.7
+ * Version:           2.1.0
  * Author:            Code of Conduct LLC
  * Author URI:        http://codeofconduct.co/
  * License:           GPL-2.0+
