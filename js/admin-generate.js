9ebeb03a4b2c21796be528f2b555ac57dbd59ce8
Patrick Posner <hello@patrickposner.dev>
2022-04-08 11:13:58 +0200

    increase ping time

--- js/admin-generate.js
+++ js/admin-generate.js
@@ -1,6 +1,6 @@
 'use strict';
-jQuery( document ).ready( function( $ ) {
-	var REFRESH_EVERY_X_SECONDS = 2;
+jQuery(document).ready(function ($) {
+	var REFRESH_EVERY_X_SECONDS = 15;
 	var STATIC_PAGES_PER_PAGE = 50; // max number of pages to show at once
 	var done = true;
 	var refreshTimer = null;
@@ -10,57 +10,57 @@ jQuery( document ).ready( function( $ ) {
 	display_activity_log();
 	initiate_action();
 
-	$( '#sistContainer #generate' ).click( function( e ) {
-		$( '#sistContainer #activityLog' ).html('');
-		initiate_action( 'start' );
-	} );
+	$('#sistContainer #generate').click(function (e) {
+		$('#sistContainer #activityLog').html('');
+		initiate_action('start');
+	});
 
-	$( '#sistContainer #cancel' ).click( function( e ) {
-		initiate_action( 'cancel' );
-	} );
+	$('#sistContainer #cancel').click(function (e) {
+		initiate_action('cancel');
+	});
 
 	// disable all actions and show spinner
-	function initiate_action( action ) {
-		if ( action == null ) {
+	function initiate_action(action) {
+		if (action == null) {
 			action = 'ping';
 		}
 
 		// cancel existing timer
-		if ( refreshTimer != null ) {
-			clearInterval( refreshTimer );
+		if (refreshTimer != null) {
+			clearInterval(refreshTimer);
 		}
 		// send action now
-		send_action_to_archive_manager( action );
+		send_action_to_archive_manager(action);
 		// set loop for pinging server
-		refreshTimer = setInterval( function() {
-			send_action_to_archive_manager( 'ping' );
-		}, REFRESH_EVERY_X_SECONDS * 1000 );
+		refreshTimer = setInterval(function () {
+			send_action_to_archive_manager('ping');
+		}, REFRESH_EVERY_X_SECONDS * 1000);
 	}
 
 	// where action is one of 'start', 'continue', 'cancel'
-	function send_action_to_archive_manager( action ) {
+	function send_action_to_archive_manager(action) {
 		var data = {
 			'_ajax_nonce': $('#_wpnonce').val(),
 			'action': 'static_archive_action',
 			'perform': action
 		};
 
-		$.post( window.ajaxurl, data, function( response ) {
-			if ( response.action == 'start' ) {
-				$( '#generate' ).attr( 'disabled', 'disabled' );
-				$( '#sistContainer .actions .spinner' ).addClass( 'is-active' );
+		$.post(window.ajaxurl, data, function (response) {
+			if (response.action == 'start') {
+				$('#generate').attr('disabled', 'disabled');
+				$('#sistContainer .actions .spinner').addClass('is-active');
 			}
-			handle_response_from_archive_manager( response );
-		} );
+			handle_response_from_archive_manager(response);
+		});
 	}
 
-	function handle_response_from_archive_manager( response ) {
+	function handle_response_from_archive_manager(response) {
 		// loop through the responses and create an .activity div for each one
 		// in #activityLog
-		var $activityLog = $( '#activityLog' );
-		$activityLog.html( response.activity_log_html )
-			.scrollTop( $activityLog.prop( 'scrollHeight' ) );
-		if ( response.done == true && done == false ) {
+		var $activityLog = $('#activityLog');
+		$activityLog.html(response.activity_log_html)
+			.scrollTop($activityLog.prop('scrollHeight'));
+		if (response.done == true && done == false) {
 			display_export_log();
 		}
 
@@ -68,20 +68,20 @@ jQuery( document ).ready( function( $ ) {
 
 		// only adjust the button/spinner state on a 'ping'
 		// (ensures that the job has had time to process the action)
-		if ( response.action == 'ping' ) {
-				if ( done == true ) {
-					// remove spinner and show #generate
-					$( '#sistContainer .actions .spinner' ).removeClass( 'is-active' );
-					$( '#sistContainer #generate' ).removeClass( 'hide' );
-					$( '#sistContainer #cancel' ).addClass( 'hide' );
-				} else {
-					// Enable cancel button.
-					$( '#sistContainer #cancel' ).removeClass( 'hide' );
+		if (response.action == 'ping') {
+			if (done == true) {
+				// remove spinner and show #generate
+				$('#sistContainer .actions .spinner').removeClass('is-active');
+				$('#sistContainer #generate').removeClass('hide');
+				$('#sistContainer #cancel').addClass('hide');
+			} else {
+				// Enable cancel button.
+				$('#sistContainer #cancel').removeClass('hide');
 
-					// Disable generate button.
-					$( '#generate' ).removeAttr( 'disabled' );
-					$( '#generate' ).addClass( 'hide' );
-				}
+				// Disable generate button.
+				$('#generate').removeAttr('disabled');
+				$('#generate').addClass('hide');
+			}
 		}
 	}
 
@@ -93,12 +93,12 @@ jQuery( document ).ready( function( $ ) {
 			'per_page': STATIC_PAGES_PER_PAGE
 		};
 
-		var $exportLog = $( '#exportLog' );
-		$exportLog.html( "<span class='spinner is-active'></span>" );
+		var $exportLog = $('#exportLog');
+		$exportLog.html("<span class='spinner is-active'></span>");
 
-		$.post( window.ajaxurl, data, function( response ) {
-			$exportLog.html( response.html );
-		} );
+		$.post(window.ajaxurl, data, function (response) {
+			$exportLog.html(response.html);
+		});
 	}
 
 	function display_activity_log() {
@@ -107,25 +107,25 @@ jQuery( document ).ready( function( $ ) {
 			'action': 'render_activity_log'
 		};
 
-		var $activityLog = $( '#activityLog' );
-		$activityLog.html( "<span class='spinner is-active'></span>" );
+		var $activityLog = $('#activityLog');
+		$activityLog.html("<span class='spinner is-active'></span>");
 
-		$.post( window.ajaxurl, data, function( response ) {
-			$activityLog.html( response.html )
-				.scrollTop( $activityLog.prop( 'scrollHeight' ) );
-		} );
+		$.post(window.ajaxurl, data, function (response) {
+			$activityLog.html(response.html)
+				.scrollTop($activityLog.prop('scrollHeight'));
+		});
 	}
 
 	// -- AJAX pagination ----------------------------------------------------//
-	$( '#sistContainer #exportLog' ).on( 'click', 'a.page-numbers', function( e ) {
+	$('#sistContainer #exportLog').on('click', 'a.page-numbers', function (e) {
 		e.preventDefault();
 
-		var url = $( this ).attr( 'href' );
+		var url = $(this).attr('href');
 		var re = /page=(\d+)/;
-		var matches = re.exec( url );
+		var matches = re.exec(url);
 
 		var page = 1;
-		if ( matches ) {
+		if (matches) {
 			page = matches[1];
 		}
 
@@ -136,9 +136,9 @@ jQuery( document ).ready( function( $ ) {
 			'per_page': STATIC_PAGES_PER_PAGE
 		};
 
-		$.post( window.ajaxurl, data, function( response ) {
-			$( '#exportLog' ).html( response.html );
-		} );
-	} );
+		$.post(window.ajaxurl, data, function (response) {
+			$('#exportLog').html(response.html);
+		});
+	});
 
-} );
+});
