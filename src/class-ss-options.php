0379f8472b22e09a875fe4cb1de18abb79e4b497
Patrick Posner <hello@patrickposner.dev>
2022-11-24 14:00:45 +0100

    added filter for zip file name

--- src/class-ss-options.php
+++ src/class-ss-options.php
@@ -136,7 +136,7 @@ class Options {
 	 * @return string The path to the temp static archive directory
 	 */
 	public function get_archive_dir() {
-		return Util::add_trailing_directory_separator( $this->get( 'temp_files_dir' ) . $this->get( 'archive_name' ) );
+		return Util::add_trailing_directory_separator( $this->get( 'temp_files_dir' ) . apply_filters( 'ss_archive_file_name', $this->get( 'archive_name' ) ) );
 	}
 
 	/**
