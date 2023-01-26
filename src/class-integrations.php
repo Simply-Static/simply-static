36016d6d037ae2267d3b7268e2d797ab8056b32a
Patrick Posner <hello@patrickposner.dev>
2023-01-26 14:41:13 +0100

    fixed merge conflict

--- src/class-integrations.php
+++ src/class-integrations.php
@@ -4,51 +4,45 @@ namespace Simply_Static;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 class Integrations {
 
-    /**
-     * @return void
-     */
-    public function load() {
-        $this->includes();
-        $integrations = $this->get_integrations();
+	/**
+	 * @return void
+	 */
+	public function load() {
+		$this->includes();
+		$integrations = $this->get_integrations();
 
-        foreach ( $integrations as $integration ) {
-            $object = new $integration();
-            $object->load();
-        }
-    }
+		foreach ( $integrations as $integration ) {
+			$object = new $integration();
+			$object->load();
+		}
+	}
 
-    public function get_integrations() {
-       return apply_filters( 'simply_static_integrations', [
-           'yoast' => Yoast_Integration::class,
-           'rank-math' => Rank_Math_Integration::class,
-           'aio-seo'   => AIO_SEO_Integration::class,
-           'seopress'  => SEOPress_Integration::class,
-           'elementor' => Elementor_Integration::class,
-<<<<<<< HEAD
-           'cookieyes' => CookieYes_Integration::class
-=======
-           'brizy'    => Brizy_Integration::class
->>>>>>> issue_61
-       ] );
-    }
+	public function get_integrations() {
+		return apply_filters( 'simply_static_integrations', [
+			'yoast'     => Yoast_Integration::class,
+			'rank-math' => Rank_Math_Integration::class,
+			'aio-seo'   => AIO_SEO_Integration::class,
+			'seopress'  => SEOPress_Integration::class,
+			'elementor' => Elementor_Integration::class,
+			'cookieyes' => CookieYes_Integration::class,
+			'brizy'     => Brizy_Integration::class
+		] );
+	}
 
-    public function includes() {
-        $path = plugin_dir_path( dirname( __FILE__ ) ) . 'src/integrations/';
-        require_once $path . 'class-integration.php';
-        require_once $path . 'class-yoast-integration.php';
-        require_once $path . 'class-rank-math-integration.php';
-        require_once $path . 'class-aio-seo-integration.php';
-        require_once $path . 'class-seopress-integration.php';
-        require_once $path . 'class-elementor-integration.php';
-<<<<<<< HEAD
-        require_once $path . 'class-cookieyes-integration.php';
-=======
-        require_once $path . 'class-brizy-integration.php';
->>>>>>> issue_61
-    }
+	public function includes() {
+		$path = plugin_dir_path( dirname( __FILE__ ) ) . 'src/integrations/';
+		require_once $path . 'class-integration.php';
+		require_once $path . 'class-yoast-integration.php';
+		require_once $path . 'class-rank-math-integration.php';
+		require_once $path . 'class-aio-seo-integration.php';
+		require_once $path . 'class-seopress-integration.php';
+		require_once $path . 'class-elementor-integration.php';
+		require_once $path . 'class-cookieyes-integration.php';
+		require_once $path . 'class-brizy-integration.php';
+	}
 }
