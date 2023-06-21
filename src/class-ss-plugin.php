4446e5a5fffcf661c416519701d0484399d5f23b
Patrick Posner <hello@patrickposner.dev>
2023-06-21 10:57:01 +0200

    updated changelog + version number

--- src/class-ss-plugin.php
+++ src/class-ss-plugin.php
@@ -15,7 +15,7 @@ class Plugin {
 	 * Plugin version
 	 * @var string
 	 */
-	const VERSION = '2.3.1';
+	const VERSION = '2.3.2';
 
 	/**
 	 * The slug of the plugin; used in actions, filters, i18n, table names, etc.
