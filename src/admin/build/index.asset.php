858f1c167cb12698301ad61467943fe7778dbedd
Patrick Posner <hello@patrickposner.dev>
2023-07-14 14:33:43 +0200

    make notifications fit

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '9d384b6c027124598185');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'b65c1d10a2eed102c83c');
