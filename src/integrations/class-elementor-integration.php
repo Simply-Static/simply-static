2ea3703038e6abb905907eba23f3fb9621a1e417
Patrick Posner <hello@patrickposner.dev>
2023-08-21 10:10:38 +0200

    #157 check if its a string before replace

--- src/integrations/class-elementor-integration.php
+++ src/integrations/class-elementor-integration.php
@@ -12,10 +12,10 @@ class Elementor_Integration extends Integration {
 	 */
 	protected $id = 'elementor';
 
-    /**
-     * @var null|Url_Extractor
-     */
-    protected $extractor = null;
+	/**
+	 * @var null|Url_Extractor
+	 */
+	protected $extractor = null;
 
 	/**
 	 * Can this integration run?
@@ -44,40 +44,46 @@ class Elementor_Integration extends Integration {
 	 */
 	public function extract_elementor_settings( $dom, $extractor ) {
 		$settings        = $dom->find( '[data-settings]' );
-        $this->extractor = $extractor;
+		$this->extractor = $extractor;
 
 		foreach ( $settings as $node ) {
-            $json    = $node->{'data-settings'};
-            $decoded = htmlspecialchars_decode( $json );
-            $decoded = json_decode( $decoded, true );
-            $decoded = $this->replace_urls_array( $decoded );
-            $node->{'data-settings'} = esc_attr( wp_json_encode( $decoded ) );
+			$json                    = $node->{'data-settings'};
+			$decoded                 = htmlspecialchars_decode( $json );
+			$decoded                 = json_decode( $decoded, true );
+			$decoded                 = $this->replace_urls_array( $decoded );
+			$node->{'data-settings'} = esc_attr( wp_json_encode( $decoded ) );
 		}
 	}
 
-    /**
-     * Replace URL in a string.
-     *
-     * @param $string
-     * @return array|string|string[]
-     */
-    public function replace_urls_in_text( $string ) {
-        $options         = \Simply_Static\Options::instance();
-        $destination_url = $options->get_destination_url();
-	    $pattern         = "/https?:\/\/[^\s\"'<]+/";
+	/**
+	 * Replace URL in a string.
+	 *
+	 * @param $string
+	 *
+	 * @return array|string|string[]
+	 */
+	public function replace_urls_in_text( $string ) {
+		// Skip if it's not a string.
+		if ( ! is_string( $string ) ) {
+			return $string;
+		}
 
-	    $string = preg_replace_callback( $pattern, array( $this, 'replace_urls' ), $string );
+		$options         = Options::instance();
+		$destination_url = $options->get_destination_url();
+		$pattern         = "/https?:\/\/[^\s\"'<]+/";
 
-	    // replace any instance of the origin url, whether it starts with https://, http://, or //.
-        $string = preg_replace( '/(https?:)?\/\/' . addcslashes( Util::origin_host(), '/' ) . '/i', $destination_url, $string );
+		$string = preg_replace_callback( $pattern, array( $this, 'replace_urls' ), $string );
 
-        // replace wp_json_encode'd urls, as used by WP's `concatemoji`.
-        // e.g. {"concatemoji":"http:\/\/www.example.org\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.6.1"}.
-        $string = str_replace( addcslashes( Util::origin_url(), '/' ), addcslashes( $destination_url, '/' ), $string );
+		// replace any instance of the origin url, whether it starts with https://, http://, or //.
+		$string = preg_replace( '/(https?:)?\/\/' . addcslashes( Util::origin_host(), '/' ) . '/i', $destination_url, $string );
+
+		// replace wp_json_encode'd urls, as used by WP's `concatemoji`.
+		// e.g. {"concatemoji":"http:\/\/www.example.org\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.6.1"}.
+		$string = str_replace( addcslashes( Util::origin_url(), '/' ), addcslashes( $destination_url, '/' ), $string );
 
 
-        return $string;
-    }
+		return $string;
+	}
 
 	public function replace_urls( $matches ) {
 		$extracted_url = $matches[0];
@@ -90,27 +96,28 @@ class Elementor_Integration extends Integration {
 		return $updated_extracted_url ? $updated_extracted_url : $extracted_url;
 	}
 
-    /**
-     * Replace URLs in an array.
-     *
-     * @param $var
-     * @return array
-     */
-    public function replace_urls_array( $var ) {
-        if ( is_array( $var ) ) {
-            return array_map( [ $this, 'replace_urls_array' ], $var );
-        } else {
-            return is_scalar( $var ) ? $this->replace_urls_in_text( $var ) : $var;
-        }
-    }
+	/**
+	 * Replace URLs in an array.
+	 *
+	 * @param $var
+	 *
+	 * @return array
+	 */
+	public function replace_urls_array( $var ) {
+		if ( is_array( $var ) ) {
+			return array_map( [ $this, 'replace_urls_array' ], $var );
+		} else {
+			return is_scalar( $var ) ? $this->replace_urls_in_text( $var ) : $var;
+		}
+	}
 
-    public function unslash_json( $var ) {
-          if ( is_array( $var ) ) {
-            return array_map( array( $this, 'unslash_json' ), $var );
-          } else {
-            return is_scalar( $var ) ? wp_unslash( $var ) : $var;
-        }
-    }
+	public function unslash_json( $var ) {
+		if ( is_array( $var ) ) {
+			return array_map( array( $this, 'unslash_json' ), $var );
+		} else {
+			return is_scalar( $var ) ? wp_unslash( $var ) : $var;
+		}
+	}
 
 	/**
 	 * Move Elementor Files to make sure all assets that might be required are there.
@@ -191,17 +198,17 @@ class Elementor_Integration extends Integration {
 	 */
 	public function register_assets() {
 
-		$file_urls   = [];
+		$file_urls = [];
 		//$bundle_urls = $this->get_bundle_files();
-		$lib_urls    = $this->get_lib_files();
+		$lib_urls = $this->get_lib_files();
 		//$file_urls   = array_merge( $file_urls, $bundle_urls );
-		$file_urls   = array_merge( $file_urls, $lib_urls );
-        $file_urls   = array_merge( $file_urls, $this->get_files_in_url( 'js' ) );
-		$file_urls   = array_merge( $file_urls, $this->get_files_in_url( 'images' ) );
-		$file_urls   = array_merge( $file_urls, $this->get_files_in_url( 'shapes' ) );
-		$file_urls   = array_merge( $file_urls, $this->get_files_in_url( 'mask-shapes' ) );
-		$file_urls   = array_merge( $file_urls, $this->get_files_in_url( 'svg-paths' ) );
-		$file_urls   = array_merge( $file_urls, $this->get_files_in_url( 'data' ) );
+		$file_urls = array_merge( $file_urls, $lib_urls );
+		$file_urls = array_merge( $file_urls, $this->get_files_in_url( 'js' ) );
+		$file_urls = array_merge( $file_urls, $this->get_files_in_url( 'images' ) );
+		$file_urls = array_merge( $file_urls, $this->get_files_in_url( 'shapes' ) );
+		$file_urls = array_merge( $file_urls, $this->get_files_in_url( 'mask-shapes' ) );
+		$file_urls = array_merge( $file_urls, $this->get_files_in_url( 'svg-paths' ) );
+		$file_urls = array_merge( $file_urls, $this->get_files_in_url( 'data' ) );
 
 		foreach ( $file_urls as $url ) {
 			Util::debug_log( 'Adding elementor bundle asset to queue: ' . $url );
