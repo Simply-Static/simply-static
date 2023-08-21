4e3b3a7f995b18c388f7557631125235bbda08c3
Patrick Posner <hello@patrickposner.dev>
2023-08-21 09:21:48 +0200

    notice for SimplyCDN

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'e5e478a4f9b085748bc2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '46f4c64d418cc476d9ab');
