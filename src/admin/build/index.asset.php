825977008221297eaea8e5e0fee308d697ad02fa
Patrick Posner <hello@patrickposner.dev>
2024-01-29 10:53:03 +0100

    fixed settings on non-multisite

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'f8a3a436843996644d9a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'd0d814c02a03f1678073');
