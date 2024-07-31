a16c03b3a2562c6db18971d5b513ec8f63c42e86
Patrick Posner <hello@patrickposner.dev>
2024-07-31 09:49:03 +0200

    #239 updated build files

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '9f85387b8e058d4c8843');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'e30138696446446cae5d');
