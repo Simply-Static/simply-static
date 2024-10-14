9176f7b327c7eb4adf1209d9cbcfc754e24cab95
Patrick Posner <hello@patrickposner.dev>
2024-10-14 12:31:08 +0200

    make clear that SFTP keys are optional

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'dae1e63617fc38fb84d1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '2796505ef03a6e559a77');
