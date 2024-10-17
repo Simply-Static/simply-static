0da5d16644c886118a93ea0ab4a2a4f2983db560
Patrick Posner <hello@patrickposner.dev>
2024-10-17 12:42:46 +0200

    CSS fix: dynamic sidebar height

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '9f00d8dbb6dc5db322be');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '11c06305e92673b0587b');
