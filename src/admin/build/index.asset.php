62d9ca747dab6578551053f6a42d47b55c706090
Patrick Posner <hello@patrickposner.dev>
2024-12-09 09:17:29 +0100

    no rounded corners in UI!

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'c34d769c05f607f077be');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '361a1d61311030bbdbee');
