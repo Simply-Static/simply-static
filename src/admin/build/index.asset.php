4817c8de2ec0fc9912091b7f1ae9ec494acd6ce6
Patrick Posner <hello@patrickposner.dev>
2025-02-05 19:52:36 +0700

    build update for UI changes

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'ae0c2b29f256b90e667a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'aac171238449800ac52c');
