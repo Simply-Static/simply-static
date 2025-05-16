983494816ae8ed375ec3ebda70bcb56712291f65
Patrick Posner <hello@patrickposner.dev>
2025-05-16 12:44:24 +0700

    #307 floating action buttons

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '4c0fbd54f8be4eb5235f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'cdca6f7a8b71e94ad44a');
