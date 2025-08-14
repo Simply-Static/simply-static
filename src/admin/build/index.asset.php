26fd04b94ec0e1b59268a76a5af03c339beafd97
Igor Benić <igor.benic@stayntouch.com>
2025-08-14 03:27:21 +0200

    Adding warning for backups on shortpixel

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'a597e62d3b7b8fa29fc6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'e5f7bcdd12c189c32402');
