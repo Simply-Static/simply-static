161c84814993f623d5130206d968b5098569baf1
Patrick Posner <hello@patrickposner.dev>
2025-08-21 15:56:24 -0300

    #336 Yoast

--- src/handlers/class-ss-yoast-sitemap-handler.php
+++ src/handlers/class-ss-yoast-sitemap-handler.php
@@ -2,6 +2,8 @@
 
 namespace Simply_Static;
 
+use Simply_Static\Options;
+
 class Yoast_Sitemap_Handler extends Page_Handler {
 
 	/**
@@ -40,6 +42,7 @@ class Yoast_Sitemap_Handler extends Page_Handler {
 	public function after_file_fetch( $destination_dir ) {
         $this->copy_xsl( $destination_dir );
         $this->rename_sitemap( $destination_dir );
+        $this->fix_sitemap_xsl_references( $destination_dir );
 	}
 
     /**
@@ -74,15 +77,71 @@ class Yoast_Sitemap_Handler extends Page_Handler {
     protected function copy_xsl( $destination_dir ) {
         $xsl_path         = dirname( WPSEO_FILE ) . '/css/main-sitemap.xsl';
         $destination_path = Util::combine_path( $destination_dir, '/main-sitemap.xsl' );
-        if ( file_exists( $destination_path ) ) {
-            return;
-        }
-        $copy = copy( $xsl_path, $destination_path );
-        if ( $copy === false ) {
-            Util::debug_log( 'Cannot copy ' . $xsl_path . ' to ' . $destination_path );
-        } else {
 
-            Util::debug_log( 'Copied ' . $xsl_path . ' to ' . $destination_path );
+        // Copy to the root directory (original behavior)
+        if ( ! file_exists( $destination_path ) ) {
+            $copy = copy( $xsl_path, $destination_path );
+            if ( $copy === false ) {
+                Util::debug_log( 'Cannot copy ' . $xsl_path . ' to ' . $destination_path );
+            } else {
+                Util::debug_log( 'Copied ' . $xsl_path . ' to ' . $destination_path );
+            }
+        }
+
+        // Also copy to the path referenced in the sitemap.xml
+        $plugin_css_dir = Util::combine_path( $destination_dir, '/wp-content/plugins/wordpress-seo/css' );
+
+        // Create directory structure if it doesn't exist
+        if ( ! file_exists( $plugin_css_dir ) ) {
+            wp_mkdir_p( $plugin_css_dir );
+        }
+
+        $plugin_xsl_path = Util::combine_path( $plugin_css_dir, '/main-sitemap.xsl' );
+
+        if ( ! file_exists( $plugin_xsl_path ) ) {
+            $copy = copy( $xsl_path, $plugin_xsl_path );
+            if ( $copy === false ) {
+                Util::debug_log( 'Cannot copy ' . $xsl_path . ' to ' . $plugin_xsl_path );
+            } else {
+                Util::debug_log( 'Copied ' . $xsl_path . ' to ' . $plugin_xsl_path );
+            }
+        }
+    }
+
+    /**
+     * Fix XSL references in sitemap XML files
+     *
+     * @param string $destination_dir Destination directory.
+     * @return void
+     */
+    protected function fix_sitemap_xsl_references( $destination_dir ) {
+        // List of sitemap files to check
+        $sitemap_files = [
+            Util::combine_path( $destination_dir, '/sitemap.xml' ),
+            Util::combine_path( $destination_dir, '/sitemap_index.xml' ),
+            // Add other sitemap files if needed
+        ];
+
+        // Find all XML files that might be sitemaps
+        $xml_files = glob( Util::combine_path( $destination_dir, '/*-sitemap.xml' ) );
+        if ( is_array( $xml_files ) ) {
+            $sitemap_files = array_merge( $sitemap_files, $xml_files );
+        }
+
+        foreach ( $sitemap_files as $sitemap_file ) {
+            if ( file_exists( $sitemap_file ) ) {
+                $content = file_get_contents( $sitemap_file );
+
+                // Replace the XSL reference with the one pointing to the root directory
+                $content = preg_replace(
+                    '/<\?xml-stylesheet type="text\/xsl" href="[^"]*wp-content\/plugins\/wordpress-seo\/css\/main-sitemap.xsl"\?>/',
+                    '<?xml-stylesheet type="text/xsl" href="' . trailingslashit( Options::instance()->get_destination_url() ) . 'main-sitemap.xsl"?>',
+                    $content
+                );
+
+                file_put_contents( $sitemap_file, $content );
+                Util::debug_log( 'Fixed XSL reference in ' . $sitemap_file );
+            }
         }
     }
 }
--- src/integrations/class-yoast-integration.php
+++ src/integrations/class-yoast-integration.php
@@ -98,6 +98,9 @@ class Yoast_Integration extends Integration {
 			$static_page->handler     = Yoast_Sitemap_Handler::class;
 			$static_page->save();
 		}
+
+		// Extract and add individual sitemap URLs from sitemap_index.xml
+		$this->extract_sitemap_urls_from_index();
 	}
 
 	/**
@@ -114,6 +117,29 @@ class Yoast_Integration extends Integration {
 
 		$urls[] = \WPSEO_Sitemaps_Router::get_base_url( 'sitemap.xml' );
 		$urls[] = \WPSEO_Sitemaps_Router::get_base_url( 'sitemap_index.xml' );
+		$urls[] = \WPSEO_Sitemaps_Router::get_base_url( 'main-sitemap.xsl' );
+
+		// Extract individual sitemap URLs from sitemap_index.xml
+		$sitemap_index_url = \WPSEO_Sitemaps_Router::get_base_url( 'sitemap_index.xml' );
+		$response = wp_remote_get( $sitemap_index_url, array( 'timeout' => 30 ) );
+
+		if ( ! is_wp_error( $response ) && wp_remote_retrieve_response_code( $response ) === 200 ) {
+			$xml_content = wp_remote_retrieve_body( $response );
+
+			// Use SimpleXML to parse the XML
+			libxml_use_internal_errors( true );
+			$xml = simplexml_load_string( $xml_content );
+
+			if ( $xml !== false && isset( $xml->sitemap ) ) {
+				foreach ( $xml->sitemap as $sitemap ) {
+					if ( isset( $sitemap->loc ) ) {
+						$sitemap_url = (string) $sitemap->loc;
+						$urls[] = $sitemap_url;
+						Util::debug_log( 'Adding individual sitemap URL to single export: ' . $sitemap_url );
+					}
+				}
+			}
+		}
 
 		return $urls;
 	}
@@ -158,5 +184,53 @@ class Yoast_Integration extends Integration {
 	 */
 	public function dependency_active() {
 		return defined( 'WPSEO_FILE' );
+	}
+
+	/**
+	 * Extract sitemap URLs from sitemap_index.xml and add them to the queue.
+	 *
+	 * @return void
+	 */
+	protected function extract_sitemap_urls_from_index() {
+		if ( ! class_exists( 'WPSEO_Sitemaps_Router' ) ) {
+			return;
+		}
+
+		$sitemap_index_url = \WPSEO_Sitemaps_Router::get_base_url( 'sitemap_index.xml' );
+		$response = wp_remote_get( $sitemap_index_url, array( 'timeout' => 30 ) );
+
+		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) !== 200 ) {
+			Util::debug_log( 'Failed to fetch sitemap index: ' . $sitemap_index_url );
+			return;
+		}
+
+		$xml_content = wp_remote_retrieve_body( $response );
+
+		// Use SimpleXML to parse the XML
+		libxml_use_internal_errors( true );
+		$xml = simplexml_load_string( $xml_content );
+
+		if ( $xml === false ) {
+			Util::debug_log( 'Failed to parse sitemap index XML: ' . $sitemap_index_url );
+			return;
+		}
+
+		// Extract sitemap URLs
+		if ( isset( $xml->sitemap ) ) {
+			foreach ( $xml->sitemap as $sitemap ) {
+				if ( isset( $sitemap->loc ) ) {
+					$sitemap_url = (string) $sitemap->loc;
+
+					// Add the sitemap URL to the queue
+					Util::debug_log( 'Adding sitemap URL to queue: ' . $sitemap_url );
+					/** @var \Simply_Static\Page $static_page */
+					$static_page = Page::query()->find_or_initialize_by( 'url', $sitemap_url );
+					$static_page->set_status_message( __( 'Sitemap URL', 'simply-static' ) );
+					$static_page->found_on_id = 0;
+					$static_page->handler     = Yoast_Sitemap_Handler::class;
+					$static_page->save();
+				}
+			}
+		}
 	}
 }
