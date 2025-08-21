6730b0c2963a7f2530b208f94e3c3b379ece2d56
Patrick Posner <hello@patrickposner.dev>
2025-08-20 22:21:10 -0300

    fixed merge conflict

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '37c9a4b60798f46a417c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '0b63106b5a78e691b54f');
