4c71cd479258fef42e0e7cd8e7e88d5a851a85d1
Patrick Posner <hello@patrickposner.dev>
2025-08-21 15:11:32 -0300

    #322 removed Jetpack integration, now covered by crawl

--- src/class-ss-integrations.php
+++ src/class-ss-integrations.php
@@ -34,7 +34,6 @@ class Integrations {
 			'cookieyes'         => CookieYes_Integration::class,
 			'complianz'         => Complianz_Integration::class,
 			//'search-and-filter' => SearchAndFilter_Integration::class,
-			'jetpack'           => Jetpack_Integration::class,
 			'multilingual'      => Multilingual_Integration::class,
 			'github'            => Github_Integration::class,
 			'shortpixel'        => Shortpixel_Integration::class,
@@ -60,7 +59,6 @@ class Integrations {
 		require_once $path . 'class-elementor-integration.php';
 		require_once $path . 'class-elementor-pro-integration.php';
 		require_once $path . 'class-cookie-yes-integration.php';
-		require_once $path . 'class-jetpack-integration.php';
 
 		// Simply Static Pro integrations.
 		require_once $path . 'class-pro-integration.php';
