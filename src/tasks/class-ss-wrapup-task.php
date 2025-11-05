d305376a687a52efb4c79786ee3efdeca94c334d
Patrick Posner <hello@patrickposner.dev>
2025-11-04 18:44:36 -0600

    added filter to clear temp dir on wrapup

--- src/tasks/class-ss-wrapup-task.php
+++ src/tasks/class-ss-wrapup-task.php
@@ -20,16 +20,29 @@ class Wrapup_Task extends Task {
 	 * @return bool
 	 */
 	public function perform() {
-		Util::debug_log( "Deleting temporary files" );
+		// Optionally clear the temporary directory (disabled by default).
+		$clear_temp_dir = apply_filters( 'ss_clear_temp_dir_on_wrapup', false );
+
+		if ( $clear_temp_dir ) {
+			try {
+				Util::debug_log( 'Clearing temporary directory on wrapup' );
+				$setup_task = new Setup_Task();
+				$deleted    = $setup_task->delete_temp_static_files();
+				do_action( 'ss_after_clear_temp_dir_on_wrapup', $deleted );
+			} catch ( \Throwable $e ) {
+				Util::debug_log( 'Error clearing temporary directory on wrapup: ' . $e->getMessage() );
+			}
+		}
+
 		$this->save_status_message( __( 'Wrapping up', 'simply-static' ) );
 
 		// Unschedule cron first.
 		wp_clear_scheduled_hook( 'simply_static_site_export_cron' );
 
 		// Clear WP object cache.
-		$flush_cashe = apply_filters( 'ss_flush_cache', true );
+		$flush_cache = apply_filters( 'ss_flush_cache', true );
 
-		if ( $flush_cashe ) {
+		if ( $flush_cache ) {
 			wp_cache_flush();
 		}
 
