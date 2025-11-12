b670b9973c7481834d80a4a4f472d8477597562a
Patrick Posner <hello@patrickposner.dev>
2025-11-12 13:10:49 -0600

    build update

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '5f0224332de8bf51b0da');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '46c09a601f19e86138d6');
