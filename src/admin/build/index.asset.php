9ce10aacbfbb5dbb901c00bfe016e7e1af777149
Patrick Posner <hello@patrickposner.dev>
2026-06-29 11:15:29 +0200

    Add environment sidebar extension slot (#388)

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '35d10d8d2bc4618c5a86');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '552ac1c7f1115d54fb86');
