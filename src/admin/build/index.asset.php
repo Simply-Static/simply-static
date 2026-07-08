10191db72a63ad412d570b479ad48f0b1b7b544f
Patrick Posner <hello@patrickposner.dev>
2026-07-08 13:48:42 +0200

    Adjust rollback notice spacing

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'f41ba5d45c2c467b14c6');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '014fe587ad44295bb884');
