14b1d06b8c4609389afe3349663bc59e566b2a75
Patrick Posner <hello@patrickposner.dev>
2026-08-05 10:08:07 +0200

    Update npm dependencies

--- assets/install-plugins/build/index.asset.php
+++ assets/install-plugins/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n'), 'version' => '2850bb7cc9bcd6a8b24c');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n'), 'version' => 'a17051761b210675628f');
