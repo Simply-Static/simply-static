2e2a17bdd3730e58720878cc4c4b0f0dae75075e
Patrick Posner <hello@patrickposner.dev>
2026-08-10 18:46:48 +0200

    Reposition Static Studio promo (#447)

--- src/admin/build/index.asset.php
+++ src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '601984472ccf9b4b13a4');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '0ef44912e3a266b0e07c');
